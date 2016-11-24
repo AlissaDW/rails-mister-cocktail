c997c0b9829dce82011d362510ae346cbab9ca52
Alissa De Wannemaeker <AlissaDeWan@gmail.com>
2016-11-24 15:28:35 +0100

    done, need to do the front-end now

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,17 +8,34 @@ class CocktailsController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail)
   end
 
   def edit
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
   end
 
   def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
   end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :doses, :ingredients)
+  end
+
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,7 @@
 class Ingredient < ApplicationRecord
 
+  NAMES = ["lemon", "ice", "mint leaves"]
+
   validates :name, presence: true, uniqueness: true
   has_many :doses
   has_many :cocktails, through: :doses
